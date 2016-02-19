975cffca734830269b30d0df644193eaa8a02978
Fawers <fabricioswerneck@gmail.com>
2016-02-19 13:12:53 -0200

    adapt to Django 1.9

--- users/models.py
+++ users/models.py
@@ -12,24 +12,32 @@ from users.utils import send_template_mail, default_token_generator
 
 
 class UserManager(BaseUserManager):
-    def _create_user(self, email, password,
-                    is_staff, is_superuser, **extra_fields):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        # python-social-auth compatibility
+        if 'username' in extra_fields:
+            del extra_fields['username']
+
         email = self.normalize_email(email)
-        user = self.model(
-            email=email, last_login=now(), date_joined=now(),
-            is_active=True, is_staff=is_staff, is_superuser=is_superuser,
-            **extra_fields
-        )
+        user = self.model(email=email, **extra_fields)
+
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
 
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, True, **extra_fields)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
